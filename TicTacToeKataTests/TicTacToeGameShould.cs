ed4c1fd30d3571b834e88f6bf65aa2712ab5ac12
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-24 08:03:30 +0100

    green test return_playerY_after_playerX_plays

--- TicTacToeKataTests/TicTacToeGameShould.cs
+++ TicTacToeKataTests/TicTacToeGameShould.cs
@@ -25,19 +25,32 @@ namespace TicTacToeKataTests {
 
             board[0, 0].Should().Be("X");
         }
+
+        [Test]
+        public void return_playerY_after_playerX_plays()
+        {
+            var ticTacToeGame = new TicTacToeGame();
+            ticTacToeGame.SetMarkOnBoard(0,1);
+
+            var playerTurn = ticTacToeGame.GetPlayerTurn();
+
+            playerTurn.Should().Be("Y");
+        }
     }
 
     public class TicTacToeGame
     {
+        private string turn = "X";
         public string GetPlayerTurn()
         {
-            return "X";
+            return turn;
         }
 
         public string[,] SetMarkOnBoard(int x, int y)
         {
             var board = new string[3, 3];
             board[0, 0] = "X";
+            turn = "Y";
             return board;
         }
     }
