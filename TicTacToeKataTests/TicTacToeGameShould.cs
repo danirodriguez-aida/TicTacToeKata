1418391ff9b1a0ab99b1d970dd9b9f927596fb89
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-24 07:35:11 +0100

    Agregar archivos de proyecto.

---
+++ TicTacToeKataTests/TicTacToeGameShould.cs
@@ -0,0 +1,12 @@
+namespace TicTacToeKataTests {
+    public class TicTacToeGameShould {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}
