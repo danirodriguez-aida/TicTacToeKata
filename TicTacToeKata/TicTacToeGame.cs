3a1f85bea4e887ce33bdfc38f2c24e39a5d6108a
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-25 07:59:59 +0100

    green test playerX_wins_for_column

--- TicTacToeKata/TicTacToeGame.cs
+++ TicTacToeKata/TicTacToeGame.cs
@@ -14,24 +14,33 @@ public class TicTacToeGame {
         return board;
     }
 
-    public string GetWinner()
-    {
+    public string GetWinner() {
         var rows = new[] { 0, 1, 2 };
+        var columns = new[] { 0, 1, 2 };
         var playerXWinInRow = rows.Any(r => IsSameSymbolForRow(r, "X"));
-        if (playerXWinInRow)
+        var playerXWinInColumn = rows.Any(r => IsSameSymbolForColumn(r, "X"));
+        if (playerXWinInRow || playerXWinInColumn)
             return "Player X";
         return string.Empty;
     }
 
-    private bool IsSameSymbolForRow(int row, string symbol)
-    {
+    private bool IsSameSymbolForRow(int row, string symbol) {
         return GetSquaresForRow(row).All(s => board.GetSymbol(s) == symbol);
     }
-
+    private bool IsSameSymbolForColumn(int column, string symbol) {
+        return GetSquaresForColumn(column).All(s => board.GetSymbol(s) == symbol);
+    }
     private static IEnumerable<Square> GetSquaresForRow(int row) {
         return new List<Square>()
         {
             new(row, 0), new (row, 1), new (row, 2)
+        };
+    }
+
+    private static IEnumerable<Square> GetSquaresForColumn(int column) {
+        return new List<Square>()
+        {
+            new(0,column), new (1,column), new (2,column)
         };
     }
 }
--- TicTacToeKataTests/TicTacToeGameShould.cs
+++ TicTacToeKataTests/TicTacToeGameShould.cs
@@ -25,8 +25,8 @@ namespace TicTacToeKataTests {
             playerTurn.Should().Be("Y");
         }
 
-        [TestCase(0,0)]
-        [TestCase(0,1)]
+        [TestCase(0, 0)]
+        [TestCase(0, 1)]
         public void set_mark_on_the_board_when_playerX_plays_coordinates(int x, int y) {
             var ticTacToeGame = new TicTacToeGame();
 
@@ -35,8 +35,8 @@ namespace TicTacToeKataTests {
             board.GetSymbol(new Square(x, y)).Should().Be("X");
         }
 
-        [TestCase(0,0)]
-        [TestCase(0,1)]
+        [TestCase(0, 0)]
+        [TestCase(0, 1)]
         public void set_mark_on_the_board_when_playerY_plays_coordinates(int x, int y) {
             var ticTacToeGame = new TicTacToeGame();
             ticTacToeGame.SetMarkOnBoard(new Square(1, 1));
@@ -70,23 +70,39 @@ namespace TicTacToeKataTests {
         [Test]
         public void without_winner() {
             var ticTacToeGame = new TicTacToeGame();
-         
+
             var winner = ticTacToeGame.GetWinner();
 
             winner.Should().Be(string.Empty);
         }
 
-        [TestCase(0,1)]
-        [TestCase(1,2)]
-        [TestCase(2,0)]
-        public void playerX_wins(int xPlayerX, int xPlayerY) {
+        [TestCase(0, 1)]
+        [TestCase(1, 2)]
+        [TestCase(2, 0)]
+        public void playerX_wins_for_row(int xPlayerX, int xPlayerY) {
             var ticTacToeGame = new TicTacToeGame();
             ticTacToeGame.SetMarkOnBoard(new Square(xPlayerX, 0));
             ticTacToeGame.SetMarkOnBoard(new Square(xPlayerY, 0));
             ticTacToeGame.SetMarkOnBoard(new Square(xPlayerX, 1));
             ticTacToeGame.SetMarkOnBoard(new Square(xPlayerY, 2));
             ticTacToeGame.SetMarkOnBoard(new Square(xPlayerX, 2));
-            
+
+            var winner = ticTacToeGame.GetWinner();
+
+            winner.Should().Be("Player X");
+        }
+
+        [TestCase(0, 1)]
+        [TestCase(1, 2)]
+        [TestCase(2, 0)]
+        public void playerX_wins_for_column(int yPlayerX, int yPlayerY) {
+            var ticTacToeGame = new TicTacToeGame();
+            ticTacToeGame.SetMarkOnBoard(new Square(0, yPlayerX));
+            ticTacToeGame.SetMarkOnBoard(new Square(0, yPlayerY));
+            ticTacToeGame.SetMarkOnBoard(new Square(1, yPlayerX));
+            ticTacToeGame.SetMarkOnBoard(new Square(2, yPlayerY));
+            ticTacToeGame.SetMarkOnBoard(new Square(2, yPlayerX));
+
             var winner = ticTacToeGame.GetWinner();
 
             winner.Should().Be("Player X");
