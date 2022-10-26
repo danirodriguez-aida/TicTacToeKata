68e3a8132995095ddde8949fc572979a679d59e3
danirodriguez <danirodriguez@AIDA-WB-7KTSPN3>
2022-10-26 07:34:48 +0100

    Refactor moving methods

--- TicTacToeKata/Board.cs
+++ TicTacToeKata/Board.cs
@@ -14,4 +14,43 @@ public class Board
     {
         return board[square.X, square.Y];
     }
+
+    public bool IsSameSymbolForRow(int row, string symbol) {
+        return Board.GetSquaresForRow(row).All(s => this.GetSymbol(s) == symbol);
+    }
+
+    public bool IsSameSymbolForColumn(int column, string symbol) {
+        return Board.GetSquaresForColumn(column).All(s => this.GetSymbol(s) == symbol);
+    }
+
+    public bool IsSameSymbolForDiagonal(bool isLeftUpToRightDown, string symbol) {
+        return Board.GetSquaresForDiagonal(isLeftUpToRightDown).All(s => this.GetSymbol(s) == symbol);
+    }
+
+    private static IEnumerable<Square> GetSquaresForRow(int row) {
+        return new List<Square>()
+        {
+            new(row, 0), new (row, 1), new (row, 2)
+        };
+    }
+
+    private static IEnumerable<Square> GetSquaresForColumn(int column) {
+        return new List<Square>()
+        {
+            new(0,column), new (1,column), new (2,column)
+        };
+    }
+
+    private static IEnumerable<Square> GetSquaresForDiagonal(bool isLeftUpToRightDown) {
+        if (isLeftUpToRightDown) {
+            return new List<Square>()
+            {
+                new(0,0), new (1,1), new (2,2)
+            };
+        }
+        return new List<Square>()
+        {
+            new(0,2), new (1,1), new (2,0)
+        };
+    }
 }
--- TicTacToeKata/TicTacToeGame.cs
+++ TicTacToeKata/TicTacToeGame.cs
@@ -16,55 +16,17 @@ public class TicTacToeGame {
 
     public string GetWinner() {
         var sizeBoard = new[] { 0, 1, 2 };
-        var playerXWinInRow = sizeBoard.Any(r => IsSameSymbolForRow(r, "X"));
-        var playerXWinInColumn = sizeBoard.Any(r => IsSameSymbolForColumn(r, "X"));
-        var playerXWinInDiagonal = IsSameSymbolForDiagonal(true, "X") || IsSameSymbolForDiagonal(false, "X");
+        var playerXWinInRow = sizeBoard.Any(r => board.IsSameSymbolForRow(r, "X"));
+        var playerXWinInColumn = sizeBoard.Any(r => board.IsSameSymbolForColumn(r, "X"));
+        var playerXWinInDiagonal = board.IsSameSymbolForDiagonal(true, "X") || board.IsSameSymbolForDiagonal(false, "X");
 
-        var playerYWinInRow = sizeBoard.Any(r => IsSameSymbolForRow(r, "Y"));
-        var playerYWinInColumn = sizeBoard.Any(r => IsSameSymbolForColumn(r, "Y"));
-        var playerYWinInDiagonal = IsSameSymbolForDiagonal(true, "Y") || IsSameSymbolForDiagonal(false, "Y");
+        var playerYWinInRow = sizeBoard.Any(r => board.IsSameSymbolForRow(r, "Y"));
+        var playerYWinInColumn = sizeBoard.Any(r => board.IsSameSymbolForColumn(r, "Y"));
+        var playerYWinInDiagonal = board.IsSameSymbolForDiagonal(true, "Y") || board.IsSameSymbolForDiagonal(false, "Y");
 
         if (playerXWinInRow || playerXWinInColumn || playerXWinInDiagonal)
             return "Player X";
         if (playerYWinInRow || playerYWinInColumn || playerYWinInDiagonal)  return "Player Y";
         return string.Empty;
-    }
-
-    private bool IsSameSymbolForRow(int row, string symbol) {
-        return GetSquaresForRow(row).All(s => board.GetSymbol(s) == symbol);
-    }
-
-    private bool IsSameSymbolForColumn(int column, string symbol) {
-        return GetSquaresForColumn(column).All(s => board.GetSymbol(s) == symbol);
-    }
-    private bool IsSameSymbolForDiagonal(bool isLeftUpToRightDown, string symbol) {
-        return GetSquaresForDiagonal(isLeftUpToRightDown).All(s => board.GetSymbol(s) == symbol);
-    }
-
-    private static IEnumerable<Square> GetSquaresForRow(int row) {
-        return new List<Square>()
-        {
-            new(row, 0), new (row, 1), new (row, 2)
-        };
-    }
-
-    private static IEnumerable<Square> GetSquaresForColumn(int column) {
-        return new List<Square>()
-        {
-            new(0,column), new (1,column), new (2,column)
-        };
-    }
-
-    private static IEnumerable<Square> GetSquaresForDiagonal(bool isLeftUpToRightDown) {
-        if (isLeftUpToRightDown) {
-            return new List<Square>()
-            {
-                new(0,0), new (1,1), new (2,2)
-            };
-        }
-        return new List<Square>()
-        {
-            new(0,2), new (1,1), new (2,0)
-        };
     }
 }
